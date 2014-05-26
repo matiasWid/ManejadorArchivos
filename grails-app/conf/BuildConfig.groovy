5ed4a545bbfc9946f7dc4e4d96e6f18221ef34f7
matiaswid <diegowidmaier2008@gmail.com>
2014-05-26 13:06:06 -0300

    Drag and drop

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,6 +59,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.3"
         compile ':cache:1.1.2'
+		compile ":uploadr:0.8.2"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
